da1f90dc32acce2df5710545d425ebde7fa8a29e
James Austrow <austrowj@users.noreply.github.com>
2026-02-13 16:21:25 -0500

    feat: add several new mechanics

--- prototypes/elevator.py
+++ prototypes/elevator.py
@@ -12,14 +12,22 @@ type Event = str
 @dataclass
 class Passenger:
     target_floor: int
-    t: int
-    #value: int
+
+@dataclass
+class Brick(Passenger): pass
+
+@dataclass
+class VIP(Passenger): pass
+
+@dataclass
+class Mechanic(Passenger): pass
 
 @dataclass
 class Elevator:
     floor: int = 0
     doors_open: bool = True
     going_to: list[int] = field(default_factory = list[int])
+    capacity: int = 0
     passengers: list[Passenger] = field(default_factory = list[Passenger])
 
 @dataclass
@@ -33,25 +41,38 @@ class Config:
 @dataclass
 class State:
     config: Config = field(default_factory = lambda: Config())
+    score: int = 0
     failed: bool = False
     time: int = 0
-    score: int = 0
+    power: int = 0
 
     floors: list[Floor] = field(default_factory = list[Floor])
     elevators: list[Elevator] = field(default_factory = list[Elevator])
+
+passenger_pool = {
+    Passenger: 10,
+    Brick: 0,
+    VIP: 3,
+    Mechanic: 1,
+}
+passenger_pool_total = sum(v for v in passenger_pool.values())
 
 def main_loop(initial_state: State):
     state = initial_state
     events = []
     while (not state.failed):
         print()
-        pprint(state, width=1)
-        if (events):
-            for event in events:
-                print(event)
+        pprint(state, width = 1)
+        for event in events:
+            print(event)
 
         action: Action = input('> ')
         state, events = handle(state, action)
+    
+    print()
+    pprint(state, width = 1)
+    for event in events:
+        print(event)
 
 def handle(state: State, action: Action) -> tuple[State, list[Event]]:
     if action == 'quit': exit()
@@ -61,17 +82,43 @@ def handle(state: State, action: Action) -> tuple[State, list[Event]]:
     elif action.startswith('go'):
         try:
             args = [int(x) for x in action.split(' ')[1:]]
-            if len(args) != 2:
-                raise Exception()
-            if args[0] >= len(state.elevators) or args[1] >= len(state.floors):
-                raise Exception()
-            if args[0] < 0 or args[1] < 0:
-                raise Exception()
+            if len(args) != 2: raise Exception()
+            if args[0] >= len(state.elevators) or args[1] >= len(state.floors): raise Exception()
+            if args[0] < 0 or args[1] < 0: raise Exception()
             
             state.elevators[args[0]].going_to.append(args[1])
 
         except Exception:
             print('Invalid "go" command, need <elevator>, <target floor>.')
+    
+    elif action.startswith('dump'):
+        try:
+            args = [int(x) for x in action.split(' ')[1:]]
+            if len(args) != 1: raise Exception()
+            if args[0] < 0 or args[0] >= len(state.elevators): raise Exception
+
+            elevator = state.elevators[args[0]]
+            if elevator.floor < len(state.floors)-1: raise Exception()
+            
+            print(f'{len(elevator.passengers)} passengers dumped into the pit.')
+            elevator.passengers = []
+
+        except Exception:
+            print(f'Invalid "dump" command, need <elevator> on floor {len(state.floors)-1}.')
+
+    elif action.startswith('close'):
+        try:
+            args = [int(x) for x in action.split(' ')[1:]]
+            if len(args) != 1: raise Exception()
+            if args[0] < 0 or args[0] >= len(state.elevators): raise Exception
+
+            elevator = state.elevators[args[0]]
+            
+            print(f"Forcibly closed elevator {args[0]}'s door.")
+            elevator.doors_open = False
+
+        except Exception:
+            print(f'Invalid "close" command, need <elevator>.')
 
     else:
         print('Invalid action.')
@@ -94,15 +141,27 @@ def update(state: State) -> tuple[State, list[Event]]:
                     getting_out = j
 
             if getting_out >= 0:
-                passenger = elevator.passengers.pop(getting_out)
-                state.score += 1
-                events.append(f'🎉 Passenger got out of elevator {i} at floor {elevator.floor} after waiting {state.time - passenger.t} minutes!')
-            elif floor.passengers:
-                elevator.passengers.append(floor.passengers.pop(0))
+                # Only the front passenger can get out.
+                passenger = elevator.passengers.pop(0)
+
+                if passenger.target_floor == elevator.floor:
+                    state, debark_events = handle_debark(state, passenger)
+                    events.append(f'🎉 Elevator {i} dropped off a passenger.')
+                    events.extend(debark_events)
+                else:
+                    # Push to front of line.
+                    floor.passengers.insert(0, passenger)
+
+            elif floor.passengers and len(elevator.passengers) < elevator.capacity:
+                # Elevator passengers are a stack.
+                elevator.passengers.insert(0, floor.passengers.pop(0))
+                if len(elevator.passengers) == elevator.capacity:
+                    events.append(f'⛔  Elevator {i} is now full.')
+
             else:
                 elevator.doors_open = False
                 if not elevator.going_to:
-                    events.append(f'❇️  Elevator {i} ready to move')
+                    events.append(f'❇️  Elevator {i} ready to move.')
         
         elif elevator.going_to:
             if elevator.going_to[0] == elevator.floor:
@@ -110,6 +169,7 @@ def update(state: State) -> tuple[State, list[Event]]:
                 elevator.doors_open = True
             elif elevator.going_to[0] < elevator.floor:
                 elevator.floor -= 1
+                state.power -= 1
             else:
                 elevator.floor += 1
     
@@ -122,15 +182,47 @@ def update(state: State) -> tuple[State, list[Event]]:
         target_floor = random.randrange(0, len(state.floors) - 1)
         if target_floor >= origin_floor:
             target_floor += 1
+        
+        # Choose among available passengers
+        passenger_roll = random.randrange(0, passenger_pool_total)
+        for PassengerConstructor, weight in passenger_pool.items():
+            if passenger_roll < weight:
+                passenger = PassengerConstructor(target_floor = target_floor)
+                break
+            else:
+                passenger_roll -= weight
+        else:
+            raise Exception('Passenger roll mechanism generated an invalid roll.')
 
-        state.floors[origin_floor].passengers.append(Passenger(target_floor = target_floor, t = state.time))
-        events.append(f'‼️  New passenger waiting at floor {origin_floor}')
+        state.floors[origin_floor].passengers.append(passenger)
+        events.append(f'‼️  New passenger waiting at floor {origin_floor}.')
 
+    if state.power <= 0:
+        state.failed = True
+        events.append(f'⚡  Power lost, game over. Final score: {state.score}')
+
+    return state, events
+
+def handle_debark(state: State, passenger: Passenger) -> tuple[State, list[Event]]:
+    events: list[Event] = []
+    if isinstance(passenger, VIP):
+        state.score += 10
+        events.append('Bonus score!')
+    elif isinstance(passenger, Mechanic):
+        state.power += 5
+        events.append('Gained power!')
+    elif isinstance(passenger, Brick):
+        state.score -= 5
+        events.append('Lost points by delivering brick.')
+    else:
+        state.score += 1
+    
     return state, events
 
 if __name__ == '__main__':
     main_loop(State(
         config = Config(float(sys.argv[3])),
+        power = 20,
         floors = [Floor() for _ in range(int(sys.argv[1]))],
-        elevators = [Elevator(floor=0) for _ in range(int(sys.argv[2]))],
+        elevators = [Elevator(floor=0, capacity=5) for _ in range(int(sys.argv[2]))],
     ))
