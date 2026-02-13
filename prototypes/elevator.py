365c916a72c8bd90746ad1e62427c82c0566ba0d
austrowj <austrowj@users.noreply.github.com>
2026-02-12 23:40:52 -0500

    feat: add error checking, notifications, and stop passengers from trying to go to their starting floor

--- prototypes/elevator.py
+++ prototypes/elevator.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+#pyright: strict
 
 from dataclasses import dataclass, field
 from pprint import pprint
@@ -6,17 +7,19 @@ import random
 import sys
 
 type Action = str
+type Event = str
 
 @dataclass
 class Passenger:
     target_floor: int
-    value: int
+    t: int
+    #value: int
 
 @dataclass
 class Elevator:
     floor: int = 0
     doors_open: bool = True
-    pressed: list[int] = field(default_factory = list[int])
+    going_to: list[int] = field(default_factory = list[int])
     passengers: list[Passenger] = field(default_factory = list[Passenger])
 
 @dataclass
@@ -39,32 +42,48 @@ class State:
 
 def main_loop(initial_state: State):
     state = initial_state
+    events = []
     while (not state.failed):
         print()
         pprint(state, width=1)
-        action: Action = input('> ')
-        state = handle(state, action)
+        if (events):
+            for event in events:
+                print(event)
 
-def handle(state: State, action: Action) -> State:
+        action: Action = input('> ')
+        state, events = handle(state, action)
+
+def handle(state: State, action: Action) -> tuple[State, list[Event]]:
     if action == 'quit': exit()
+
     elif action == '': return update(state)
-    elif action.startswith('press'):
+    
+    elif action.startswith('go'):
         try:
             args = [int(x) for x in action.split(' ')[1:]]
-            state.elevators[args[0]].pressed.append(args[1])
+            if len(args) != 2:
+                raise Exception()
+            if args[0] >= len(state.elevators) or args[1] >= len(state.floors):
+                raise Exception()
+            if args[0] < 0 or args[1] < 0:
+                raise Exception()
+            
+            state.elevators[args[0]].going_to.append(args[1])
+
         except Exception:
-            print('Invalid "press" command, need <floor>, <elevator>, <target floor>.')
+            print('Invalid "go" command, need <elevator>, <target floor>.')
 
     else:
         print('Invalid action.')
     
-    return state
+    return state, []
 
-def update(state: State) -> State:
+def update(state: State) -> tuple[State, list[Event]]:
+    events: list[Event] = []
     state.time += 1
 
     # Move passengers out of elevators
-    for _, elevator in enumerate(state.elevators):
+    for i, elevator in enumerate(state.elevators):
 
         if elevator.doors_open:
             floor = state.floors[elevator.floor]
@@ -75,29 +94,37 @@ def update(state: State) -> State:
                     getting_out = j
 
             if getting_out >= 0:
-                state.score += elevator.passengers.pop(getting_out).value
+                passenger = elevator.passengers.pop(getting_out)
+                state.score += 1
+                events.append(f'🎉 Passenger got out of elevator {i} at floor {elevator.floor} after waiting {state.time - passenger.t} minutes!')
             elif floor.passengers:
                 elevator.passengers.append(floor.passengers.pop(0))
             else:
                 elevator.doors_open = False
         
-        elif elevator.pressed:
-            if elevator.pressed[0] == elevator.floor:
-                elevator.pressed.pop(0)
+        elif elevator.going_to:
+            if elevator.going_to[0] == elevator.floor:
+                elevator.going_to.pop(0)
                 elevator.doors_open = True
-            elif elevator.pressed[0] < elevator.floor:
+            elif elevator.going_to[0] < elevator.floor:
                 elevator.floor -= 1
             else:
                 elevator.floor += 1
     
     # Randomly add passengers
     if random.random() < state.config.arrival_rate:
-        random.choice(state.floors).passengers.append(Passenger(
-            target_floor = random.randrange(0, len(state.floors)),
-            value = 1
-        ))
 
-    return state
+        origin_floor = random.randrange(0, len(state.floors))
+
+        # Choose among the other floors at random
+        target_floor = random.randrange(0, len(state.floors) - 1)
+        if target_floor >= origin_floor:
+            target_floor += 1
+
+        state.floors[origin_floor].passengers.append(Passenger(target_floor = target_floor, t = state.time))
+        events.append(f'🛗  New passenger waiting at floor {origin_floor}')
+
+    return state, events
 
 if __name__ == '__main__':
     main_loop(State(
